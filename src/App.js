ef076c5db42e6c42056adcc9d246d58dfbc71aa4
Guillaume Sere <guillaumesere@hotmail.fr>
2022-12-03 16:43:51 +0100

    modif css

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import Radio from "./Radio";
 function App() {
   return (
     <div className="App">
+        <div className="banner">
         <h1>Live Radio Player</h1>
         <h2>Choisissez votre station préférée</h2>
+        </div>
         <Radio />
     </div>
   );
--- src/Radio.js
+++ src/Radio.js
@@ -5,93 +5,95 @@ import "react-h5-audio-player/lib/styles.css";
 import defaultImage from "./radio.jpg";
 
 export default function Radio() {
-  const [stations, setStations] = useState();
-  const [stationFilter, setStationFilter] = useState("all");
+    
+    const [stations, setStations] = useState();
+    const [stationFilter, setStationFilter] = useState("all");
 
-  useEffect(() => {
-    setupApi(stationFilter).then((data) => {
-      console.log(data);
-      setStations(data);
-    });
-  }, [stationFilter]);
+    useEffect(() => {
+        setupApi(stationFilter).then((data) => {
+            console.log(data);
+            setStations(data);
+        });
+    }, [stationFilter]);
 
-  const setupApi = async (stationFilter) => {
-    const api = new RadioBrowserApi(fetch.bind(window), "My Radio App");
+    const setupApi = async (stationFilter) => {
+        const api = new RadioBrowserApi(fetch.bind(window), "My Radio App");
 
-    const stations = await api
-      .searchStations({
-        language: "english",
-        tag: stationFilter,
-        limit: 40,
-      })
-      .then((data) => {
-        return data;
-      });
+        const stations = await api
+            .searchStations({
+                language: "english",
+                tag: stationFilter,
+                limit: 40,
+            })
+            .then((data) => {
+                return data;
+            });
 
-    return stations;
-  };
+        return stations;
+    };
 
-  const filters = [
-    "all",
-    "classical",
-    "country",
-    "dance",
-    "disco",
-    "house",
-    "jazz",
-    "pop",
-    "rap",
-    "retro",
-    "rock",
-  ];
+    const filters = [
+        "all",
+        "classical",
+        "country",
+        "dance",
+        "disco",
+        "house",
+        "jazz",
+        "pop",
+        "rap",
+        "retro",
+        "rock",
+    ];
 
-  const setDefaultSrc = (event) => {
-    event.target.src = defaultImage;
-  };
+    const setDefaultSrc = (event) => {
+        event.target.src = defaultImage;
+    };
 
-  return (
-    <div className="radio">
-      <div className="filters">
-        {filters.map((filter) => (
-          <span
-            className={stationFilter === filter ? "selected" : ""}
-            onClick={() => setStationFilter(filter)}
-          >
-            {filter}
-          </span>
-        ))}
-      </div>
-      <div className="stations">
-        {stations &&
-          stations.map((station, index) => {
-            return (
-              <div className="station" key={index}>
-                <div className="stationName">
-                  <img
-                    className="logo"
-                    src={station.favicon}
-                    alt="station logo"
-                    onError={setDefaultSrc}
-                  />
-                  <div className="name">{station.name}</div>
-                </div>
+    return (
+        <div className="radio">
+            <div className="filters">
+                {filters.map((filter, index) => (
+                    <span
+                        key={index}
+                        className={stationFilter === filter ? "selected" : ""}
+                        onClick={() => setStationFilter(filter)}
+                    >
+                        {filter}
+                    </span>
+                ))}
+            </div>
+            <div className="stations">
+                {stations &&
+                    stations.map((station, index) => {
+                        return (
+                            <div className="station" key={index}>
+                                <div className="stationName">
+                                    <img
+                                        className="logo"
+                                        src={station.favicon}
+                                        alt="station logo"
+                                        onError={setDefaultSrc}
+                                    />
+                                    <div className="name">{station.name}</div>
+                                </div>
 
-                <AudioPlayer
-                  className="player"
-                  src={station.urlResolved}
-                  showJumpControls={false}
-                  layout="stacked"
-                  customProgressBarSection={[]}
-                  customControlsSection={["MAIN_CONTROLS", "VOLUME_CONTROLS"]}
-                  autoPlayAfterSrcChange={false}
-                />
-              </div>
-            );
-          })}
-      </div>
-      <div className="footer">
-        <p> Copyrights 2022, Developed by <span>Guillaume SERE </span>with React</p>
-      </div>
-    </div>
-  );
+                                <AudioPlayer
+                                    className="player"
+                                    src={station.urlResolved}
+                                    showJumpControls={false}
+                                    layout="stacked"
+                                    customProgressBarSection={[]}
+                                    customControlsSection={["MAIN_CONTROLS", "VOLUME_CONTROLS"]}
+                                    autoPlayAfterSrcChange={false}
+                                />
+                            </div>
+                        );
+                    })}
+            </div>
+            <div className="footer">
+                <p> Copyrights 2022, Developed by <span>Guillaume SERE </span>with React</p>
+            </div>
+        </div>
+    );
 }
