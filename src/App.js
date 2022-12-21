dd74dfccf908143feec21951da96cd7798e7efe2
Guillaume Sere <guillaumesere@hotmail.fr>
2022-12-21 16:13:40 +0100

    modif css

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
 import "./App.css";
 import Radio from "./Radio";
-import guirlande1 from "./light.png"
-import guirlande2 from "./light1.png"
+import DayNightToggle from 'react-day-and-night-toggle'
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-        <div className="banner">
-        <img className="guirlande" src={guirlande1} alt="" />
-        <img className="guirlande1" src={guirlande2} alt="" />
-        <img className="guirlande2" src={guirlande2} alt="" />
-        <img className="guirlande3" src={guirlande2} alt="" />
-        <img className="guirlande4" src={guirlande2} alt="" />
-        <img className="guirlande5" src={guirlande2} alt="" />
-        <h1>Live Radio Player</h1>
-        <h2>Choisissez votre station préférée</h2>
-        <img className="guirlande6" src={guirlande1} alt="" />
-        <img className="guirlande7" src={guirlande2} alt="" />
-        <img className="guirlande8" src={guirlande2} alt="" />
-        <img className="guirlande9" src={guirlande2} alt="" />
-        <img className="guirlande10" src={guirlande2} alt="" />
-        <img className="guirlande11" src={guirlande2} alt="" />
+
+    const [isDarkMode, setIsDarkMode] = useState(localStorage.getItem('data-theme') === 'dark' ? true : false)
+
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+      const newColorScheme = e.matches ? 'dark' : 'light'
+  
+      setIsDarkMode(newColorScheme === 'dark' ? true : false)
+      localStorage.setItem('data-theme', newColorScheme)
+      document.body.setAttribute('data-theme', localStorage.getItem('data-theme'))
+    })
+  
+    const handleChangeTheme = () => {
+      setIsDarkMode(!isDarkMode)
+      if(!isDarkMode) {
+        localStorage.setItem('data-theme', 'dark')
+        document.body.setAttribute('data-theme', 'dark')
+      } else {
+        localStorage.setItem('data-theme', 'light')
+        document.body.setAttribute('data-theme', 'light')
+      }
+    }
+
+    return (
+        <div className="App">
+            <div className="banner">
+                <h1>SEARCH RADIO</h1>
+                <DayNightToggle
+                    onChange={handleChangeTheme}
+                    checked={isDarkMode}
+                />
+            </div>
+            <Radio />
         </div>
-        <Radio />
-    </div>
-  );
+    );
 }
 
 export default App;
